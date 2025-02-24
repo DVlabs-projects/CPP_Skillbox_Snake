d08084ddc425da0ae7c7faa914aac67a285adf6a
Dmitriy Telnov <dimeant@mail.ru>
2025-02-24 20:47:23 +0900

    Добавлены файлы проекта

---
+++ snake/build/CMakeFiles/snake.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/Proj_C++/module8/snake/src/Snake.cpp" "CMakeFiles/snake.dir/src/Snake.cpp.obj" "gcc" "CMakeFiles/snake.dir/src/Snake.cpp.obj.d"
+  "D:/Proj_C++/module8/snake/src/main.cpp" "CMakeFiles/snake.dir/src/main.cpp.obj" "gcc" "CMakeFiles/snake.dir/src/main.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
